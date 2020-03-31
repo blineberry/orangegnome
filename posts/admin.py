0f00c9b220e8d296ef6005d09e900e9b1cd63e18
Brent Lineberry <brent.lineberry@emory.edu>
2020-03-30 22:37:39 -0400

    added twitter app. moved post publish to a custom button.

--- posts/admin.py
+++ posts/admin.py
@@ -5,16 +5,26 @@ from django.utils import timezone
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = { 'slug': ('title',)}
     readonly_fields = ('published',)
+    exclude = ('is_published',)
+
+    def _unpublish(self, obj):
+        obj.is_published = False
+        return obj
+
+    def _publish(self, obj):
+        if obj.is_published:
+            return obj
+
+        obj.is_published = True
+        obj.published = timezone.now()
+        return obj
 
     def save_model(self, request, obj, form, change):
-        if not obj.is_published:
-            obj.published = None
-            return super().save_model(request, obj, form, change)
-        
-        if 'is_published' not in form.changed_data:
-            return super().save_model(request, obj, form, change)
-        
-        obj.published = timezone.now()
+        if '_unpublish' in request.POST:
+            obj = self._unpublish(obj)
+
+        if '_publish' in request.POST:
+            obj = self._publish(obj)
 
         return super().save_model(request, obj, form, change)
 
