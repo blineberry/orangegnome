751ee7b5795cdfdb11d692cbf0b9d8ae18f3d051
Brent Lineberry <brent.lineberry@emory.edu>
2022-11-22 16:12:02 -0500

    Added a ModelForm for NoteAdmin to display Note Content with a textarea instead of an input[type=text]

--- notes/admin.py
+++ notes/admin.py
@@ -4,10 +4,49 @@ from syndications.admin import SyndicatableAdmin
 from feed.admin import PublishableAdmin
 from webmentions.admin import WebmentionAdmin
 from webmentions.models import Webmention
+from django.forms import ModelForm, CharField, Textarea
 
-# Register your models here.
+# Customize the Admin form
+class NoteModelForm(ModelForm):
+    """
+    Customizations for the Add and Change admin pages.
+
+    Inherits from forms.ModelForm.
+    """
+
+    content = CharField(widget=Textarea)
+    """Display the content input as a Textarea"""
+
+    class Meta:
+        model = Note
+        fields = [
+            'content',
+            'in_reply_to',
+            'author',
+            'tags',
+            'syndicate_to_twitter', 
+            'syndicated_to_twitter',
+            'is_published',
+            'published'
+        ]
+
+# Admin specs for the Note model
 class NoteAdmin(PublishableAdmin, SyndicatableAdmin, WebmentionAdmin):
+    """
+    Specifications for the Note Admin page.
+
+    Inherits from PublishableAdmin, SyndicatableAdmin, and WebmentionAdmin
+    """
+
+    form = NoteModelForm
+    """Override the dynamically created form with customizations."""
+
     readonly_fields = ('published','syndicated_to_twitter')
+    """
+    These fields will be shown but uneditable. 
+    
+    https://docs.djangoproject.com/en/4.1/ref/contrib/admin/#django.contrib.admin.ModelAdmin.readonly_fields
+    """
 
     fieldsets = (
         (None, {
@@ -20,10 +59,31 @@ class NoteAdmin(PublishableAdmin, SyndicatableAdmin, WebmentionAdmin):
             'fields': ('is_published','published')
         })
     )
+    """
+    Group related fields together. 
+    
+    https://docs.djangoproject.com/en/4.1/ref/contrib/admin/#django.contrib.admin.ModelAdmin.fieldsets
+    """
 
     filter_horizontal = ('tags',)
+    """
+    Use the built-in interface for the tags many-to-many relationship.
+    
+    https://docs.djangoproject.com/en/4.1/ref/contrib/admin/#django.contrib.admin.ModelAdmin.filter_horizontal
+    """
 
     def get_links_to_webmention(self, request, obj, form, change):
+        """
+        Called from save_model, returns a list of urls from the Note content.
+
+        Has all the information available in save_model:
+        request     =   the HttpRequest
+        obj         =   the Note instance
+        form        =   the ModelForm instance
+        change      =   boolean value based on whether the object is being added or 
+                        changed
+        """
+
         content_links = Webmention.get_links_from_text(obj.content)
 
         if change:
@@ -37,6 +97,10 @@ class NoteAdmin(PublishableAdmin, SyndicatableAdmin, WebmentionAdmin):
         return content_links
 
     def should_send_webmentions(self, request, obj, form, change):
+        """
+        Given the save_model context, returns a boolean if webmentions should 
+        be sent or not.
+        """
         return obj.is_published
 
 admin.site.register(Note, NoteAdmin)
