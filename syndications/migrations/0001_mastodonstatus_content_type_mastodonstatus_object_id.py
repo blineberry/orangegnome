7615bbb8ebb619e069b4e39de0abcf11ee8d9e49
Brent Lineberry <brent@thelineberrys.com>
2022-12-09 22:20:07 -0500

    errors with the default=0 in prod migration

--- syndications/migrations/0001_mastodonstatus_content_type_mastodonstatus_object_id.py
+++ syndications/migrations/0001_mastodonstatus_content_type_mastodonstatus_object_id.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='mastodonstatus',
             name='content_type',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to='contenttypes.contenttype'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='contenttypes.contenttype'),
             preserve_default=False,
         ),
         migrations.AddField(
