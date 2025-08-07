43a7e115276d235f3320b0644479a48284e0821a
Brent Lineberry <brent@thelineberrys.com>
2025-08-07 16:22:36 -0400

    attempt at fix for index on post title sql migration error

--- posts/models.py
+++ posts/models.py
@@ -25,7 +25,7 @@ class Post(TwitterSyndicatable, MastodonSyndicatable, FeedItem):
     summary_md = models.TextField(help_text="Markdown supported.")
 
     title_max = 100
-    title_md = models.TextField(unique=True)
+    title_md = models.TextField(help_text="Markdown supported.")
 
     content_md = models.TextField(help_text="Markdown supported.")    
     category = models.ForeignKey(Category, on_delete=models.PROTECT,related_name='posts', null=True, blank=True)
