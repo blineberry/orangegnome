29ab76b9d7aa55437fbf0c6d292aa16e8a029910
Brent Lineberry <brent@thelineberrys.com>
2025-08-21 14:02:25 -0400

    likes squashed to normal

--- likes/migrations/0001_squashed_0002_like_syndicate_to_mastodon_and_more.py
+++ likes/migrations/0001_squashed_0002_like_syndicate_to_mastodon_and_more.py
@@ -6,8 +6,6 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    replaces = [('likes', '0001_initial'), ('likes', '0002_like_syndicate_to_mastodon_and_more')]
-
     initial = True
 
     dependencies = [
