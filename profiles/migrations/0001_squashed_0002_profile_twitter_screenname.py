6040683e73f91781b0f54dd506d2774a0ba1c4a5
Brent Lineberry <brent@thelineberrys.com>
2025-08-21 14:12:42 -0400

    profiles squashed to normal

--- profiles/migrations/0001_squashed_0002_profile_twitter_screenname.py
+++ profiles/migrations/0001_squashed_0002_profile_twitter_screenname.py
@@ -6,8 +6,6 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    replaces = [('profiles', '0001_initial'), ('profiles', '0002_profile_twitter_screenname')]
-
     initial = True
 
     dependencies = [
