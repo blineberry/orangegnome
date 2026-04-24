64d4c4d45b38771a6a83fd5a462641499410984d
Brent Lineberry <brent@thelineberrys.com>
2026-04-23 21:54:55 -0400

    fixed feed issues

--- feed/feed.py
+++ feed/feed.py
@@ -4,11 +4,11 @@ from django.conf import settings
 from django.utils import timezone
 
 class LatestEntriesFeed(Feed):
-    title = "Orange Gnome"
+    title = "Brent Lineberry"
     link = settings.SITE_URL
     description = "Latest entries from Brent Lineberry."
     author_name = "Brent Lineberry"
-    author_link = "https://orangegnome.com"
+    author_link = "https://www.brentlineberry.com"
     
     def feed_copyright(self):
         return f'Copyright (c) {timezone.now().year} Brent Lineberry'
@@ -17,13 +17,13 @@ class LatestEntriesFeed(Feed):
         return FeedItem.objects.filter(published__lte=timezone.now()).exclude(like__isnull=False).exclude(in_reply_to__isnull=False).exclude(in_reply_to='').order_by('-published')[:10]
     
     def item_title(self, item):
-        return item.get_child().feed_item_header()
+        return item.feed_item_header()
     
     def item_description(self, item):
-        return item.get_child().feed_item_content()
+        return item.feed_item_content()
 
     def item_link(self, item):
-        return item.get_child().feed_item_link()
+        return item.feed_item_link()
     
     def item_pubdate(self, item):
         return item.published
