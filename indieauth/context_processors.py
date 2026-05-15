dc41031c902459338dc978c1b7037ef69432d454
Brent Lineberry <brent@thelineberrys.com>
2026-05-15 15:13:21 -0400

    fix for indieauth metadata url

--- indieauth/context_processors.py
+++ indieauth/context_processors.py
@@ -5,7 +5,7 @@ def indieauth(request):
     if request.path == '/':
         return { 'indieauth': 
             {
-                'metadata': settings.INDIEAUTH_TOKEN,
+                'metadata': settings.INDIEAUTH_METADATA,
                 'auth': settings.INDIEAUTH_AUTH, 
                 'token': settings.INDIEAUTH_TOKEN, 
             }
