239a8313fcdf3cd4a92a61ecb24244e92ee0c9c0
Brent Lineberry <brent@thelineberrys.com>
2026-05-18 13:50:41 -0400

    indieauth admin changes

--- indieauth/admin.py
+++ indieauth/admin.py
@@ -1,21 +1,35 @@
+from django import forms
 from django.contrib import admin
 
 from indieauth.models import AccessToken, AuthCode, RefreshToken
 
+
+class CodeForm(forms.ModelForm):
+    code = forms.CharField(widget=forms.PasswordInput(render_value=True))
+
+class TokenForm(forms.ModelForm):
+    token = forms.CharField(widget=forms.PasswordInput(render_value=True))
+
 # Register your models here.
 class AuthCodeAdmin(admin.ModelAdmin):
+    form = CodeForm
+    
     readonly_fields = ['generate_token']
     list_display = ["client_id", "user", "is_expired"]
     list_filter = ["user"]
     search_fields = ["client_id"]
 
 class AccessTokenAdmin(admin.ModelAdmin):
+    form = TokenForm
+
     readonly_fields = ['generate_token']
     list_display = ["client_id", "user", "is_expired", "expires_in"]
     list_filter = ["user", "expires_utc"]
     search_fields = ["client_id"]
 
 class RefreshTokenAdmin(admin.ModelAdmin):
+    form = TokenForm
+    
     readonly_fields = ['generate_token']
     list_display = ["client_id", "user", "is_expired", "expires_in"]
     list_filter = ["user", "expires_utc"]
